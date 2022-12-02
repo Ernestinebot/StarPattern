6a553c5a0a531cf6ea099d981da9ab1eae921641
Ernestinebot <116901955+Ernestinebot@users.noreply.github.com>
2022-12-02 11:22:15 -0500

    Initial commit

---
+++ src/starpattern/StarPattern.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package starpattern;
+
+/**
+ *
+ * @author ernestine. Practice sout tab and System.out.println and understanding this basic block.
+ */
+public class StarPattern {   //File must be saved as StarPattern.java
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("    *    ");
+         System.out.println("* *   * * ");
+         System.out.println("   * *   ");
+         System.out.println("  *   *  ");
+         System.out.println(" *     * ");
+         System.out.println();
+    }
+    
+}
